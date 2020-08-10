9525ad480f9890066f47dde842b33adcd8320a1f
UCO <ucovery@gmail.com>
2020-08-10 03:30:37 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,17 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"net/http"
 	"os"
 )
 
 func main() {
-	addr, err := determineListenAddress()
-	if err != nil {
-		log.Fatal(err)
-	}
 
 	http.HandleFunc("/", HomePage)
 
@@ -29,17 +23,6 @@ func main() {
 	http.HandleFunc("/deleteall", DeleteAllUserTokens)
 
 	//Запускаем сервер на порте 8080
+	http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 
-	log.Printf("Listening on %s...\n", addr)
-	if err := http.ListenAndServe(addr, nil); err != nil {
-		panic(err)
-	}
-}
-
-func determineListenAddress() (string, error) {
-	port := os.Getenv("PORT")
-	if port == "" {
-		return "", fmt.Errorf("$PORT not set")
-	}
-	return ":" + port, nil
 }
