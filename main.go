70e20d36e86115738a9162fc25f24a5354beedd9
UCO <ucovery@gmail.com>
2020-08-10 03:39:40 +0300

    Update main.go

--- main.go
+++ main.go
@@ -8,21 +8,21 @@ import (
 func main() {
 
 	http.HandleFunc("/", HomePage)
+	/*
+		//Определяем маршруты
+		//Маршрут выдает пару access-refresh токенов
+		http.HandleFunc("/signin", Signin)
 
-	//Определяем маршруты
-	//Маршрут выдает пару access-refresh токенов
-	http.HandleFunc("/signin", Signin)
+		//Маршрут обновляет пару access-refresh токенов
+		http.HandleFunc("/refresh", Refresh)
 
-	//Маршрут обновляет пару access-refresh токенов
-	http.HandleFunc("/refresh", Refresh)
+		//Маршрут удаляет конкретный токен
+		http.HandleFunc("/deleteone", DeleteCurrentToken)
 
-	//Маршрут удаляет конкретный токен
-	http.HandleFunc("/deleteone", DeleteCurrentToken)
+		//Маршрут удаляет все токены выданные конекретному guid
+		http.HandleFunc("/deleteall", DeleteAllUserTokens)
+	*/
 
-	//Маршрут удаляет все токены выданные конекретному guid
-	http.HandleFunc("/deleteall", DeleteAllUserTokens)
-
-	//Запускаем сервер на порте 8080
 	http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 
 }
